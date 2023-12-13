b6f22a3ee04c38bbc3553450fd148b335967f902
spikerrbg <val_05@abv.bg>
2023-12-14 01:14:51 +0200

    fix footer

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Footer(){
     return(
-        <div className="fixed bottom-0 w-full pt-2 px-4 border-t
+        <div className="fixed bottom-0 w-full pt-2 p-4 border-t
        bg-slate-100">
             <div className="md:max-w-screen-2xl mx-auto flex items-start w-full justify-between">
             <Logo />
@@ -13,7 +13,7 @@ export default function Footer(){
                     Privacy Policy
                 </Button>
                 <Button size="sm" variant="ghost">
-                    Terms of
+                    Terms of Service
                 </Button>
             </div>
             </div>
