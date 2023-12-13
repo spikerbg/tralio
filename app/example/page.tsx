fb977520634214f8b00979c8606e9134c6a9d6d8
spikerrbg <val_05@abv.bg>
2023-12-13 17:59:53 +0200

    Initial commit

---
+++ app/example/page.tsx
@@ -0,0 +1,7 @@
+export default function ExamplePage(){
+    return(
+        <div>
+            Example page
+        </div>
+    )
+}
---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function IdPage(){
+    return(
+        <div>
+            Id page!
+        </div>
+    )
+}
