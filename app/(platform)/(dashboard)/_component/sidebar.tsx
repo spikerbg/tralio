7350f262a5d9a7e8d7629b2983b383b58873ef7a
spikerrbg <val_05@abv.bg>
2023-12-14 17:41:25 +0200

    update sidebar

--- app/(platform)/(dashboard)/_component/sidebar.tsx
+++ app/(platform)/(dashboard)/_component/sidebar.tsx
@@ -1,6 +1,47 @@
 "use client"
 
-export const Sidebar = () =>{
+import Link from "next/link"
+import { Plus } from "lucide-react"
+import { useLocalStorage } from "usehooks-ts"
+import { useOrganization, useOrganizationList } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
+interface SidebarProps {
+  storageKey?: string
+}
+
+export const Sidebar = ({
+  storageKey = "t-sidebar-state",
+}: SidebarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const {
+    organization: activeOrganization,
+    isLoaded: isLoadedOrg
+  } = useOrganization();
+  const { 
+    userMemberships,
+    isLoaded: isLoadedOrgList
+  } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded)
+    .reduce((acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+  }, []);
   return(
     <div>
     Sidebar
